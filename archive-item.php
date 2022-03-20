0e95423b12ef93ce23c81fd40825497129828e5d
takumi <t.takata121@outlook.jp>
2022-03-20 10:01:30 +0900

    #58

--- archive-item.php
+++ archive-item.php
@@ -1,11 +1,6 @@
 <?php
 /**
- * The main template file
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * The template for displaying archive pages
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
@@ -17,17 +12,13 @@ get_header();
 
 	<main id="primary" class="site-main col-md-8">
 
-		<?php
-		if ( have_posts() ) :
+		<?php if ( have_posts() ) : ?>
 
-			if ( is_home() && ! is_front_page() ) :
-				?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
-				<?php
-			endif;
+			<header class="page-header">
+				<h1>takuchan shop</h1>
+			</header><!-- .page-header -->
 
+			<?php
 			/* Start the Loop */
 			while ( have_posts() ) :
 				the_post();
--- functions.php
+++ functions.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * takuchan__s functions and definitions
  *
@@ -7,9 +8,28 @@
  * @package takuchan__s
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
+add_action('init', function () {
+	register_post_type('item', [
+		'label' => '商品',
+		'public' => true,//表示するかどうか
+		'menu_position' => 5,//表示位置
+		'menu_icon' => 'dashicons-store',//アイコン
+		'supports' => ['thumbnail', 'title', 'editor', 'page-attributes'],//thumbnail(アイキャッチ)を使えるようにする
+		'has_archive' => true, //一覧表示（設定からパーマリンク更新が必要）
+		// 'hierarchical' => true //階層構造を持たせる(supportsにattributesが必要)、チェックボックスで選ぶことができるようになる
+		'show_in_rest' => true, //ブロックエディタ（REST API）有効化
+	]);
+	register_taxonomy('genre', 'item', [
+		'label' => '商品ジャンル',
+		'hierarchical' => true,
+		'show_in_rest' => true,
+	]);
+});
+
+
+if (!defined('_S_VERSION')) {
 	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
+	define('_S_VERSION', '1.0.0');
 }
 
 /**
@@ -19,17 +39,18 @@ if ( ! defined( '_S_VERSION' ) ) {
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function takuchan__s_setup() {
+function takuchan__s_setup()
+{
 	/*
 		* Make theme available for translation.
 		* Translations can be filed in the /languages/ directory.
 		* If you're building a theme based on takuchan__s, use a find and replace
 		* to change 'takuchan__s' to the name of your theme in all the template files.
 		*/
-	load_theme_textdomain( 'takuchan__s', get_template_directory() . '/languages' );
+	load_theme_textdomain('takuchan__s', get_template_directory() . '/languages');
 
 	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
+	add_theme_support('automatic-feed-links');
 
 	/*
 		* Let WordPress manage the document title.
@@ -37,19 +58,19 @@ function takuchan__s_setup() {
 		* hard-coded <title> tag in the document head, and expect WordPress to
 		* provide it for us.
 		*/
-	add_theme_support( 'title-tag' );
+	add_theme_support('title-tag');
 
 	/*
 		* Enable support for Post Thumbnails on posts and pages.
 		*
 		* @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
 		*/
-	add_theme_support( 'post-thumbnails' );
+	add_theme_support('post-thumbnails');
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus(
 		array(
-			'menu-1' => esc_html__( 'Primary', 'takuchan__s' ),
+			'menu-1' => esc_html__('Primary', 'takuchan__s'),
 		)
 	);
 
@@ -83,7 +104,7 @@ function takuchan__s_setup() {
 	);
 
 	// Add theme support for selective refresh for widgets.
-	add_theme_support( 'customize-selective-refresh-widgets' );
+	add_theme_support('customize-selective-refresh-widgets');
 
 	/**
 	 * Add support for core custom logo.
@@ -100,7 +121,7 @@ function takuchan__s_setup() {
 		)
 	);
 }
-add_action( 'after_setup_theme', 'takuchan__s_setup' );
+add_action('after_setup_theme', 'takuchan__s_setup');
 
 /**
  * Set the content width in pixels, based on the theme's design and stylesheet.
@@ -109,22 +130,24 @@ add_action( 'after_setup_theme', 'takuchan__s_setup' );
  *
  * @global int $content_width
  */
-function takuchan__s_content_width() {
-	$GLOBALS['content_width'] = apply_filters( 'takuchan__s_content_width', 640 );
+function takuchan__s_content_width()
+{
+	$GLOBALS['content_width'] = apply_filters('takuchan__s_content_width', 640);
 }
-add_action( 'after_setup_theme', 'takuchan__s_content_width', 0 );
+add_action('after_setup_theme', 'takuchan__s_content_width', 0);
 
 /**
  * Register widget area.
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function takuchan__s_widgets_init() {
+function takuchan__s_widgets_init()
+{
 	register_sidebar(
 		array(
-			'name'          => esc_html__( 'Sidebar', 'takuchan__s' ),
+			'name'          => esc_html__('Sidebar', 'takuchan__s'),
 			'id'            => 'sidebar-1',
-			'description'   => esc_html__( 'Add widgets here.', 'takuchan__s' ),
+			'description'   => esc_html__('Add widgets here.', 'takuchan__s'),
 			'before_widget' => '<section id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</section>',
 			'before_title'  => '<h2 class="widget-title">',
@@ -132,22 +155,23 @@ function takuchan__s_widgets_init() {
 		)
 	);
 }
-add_action( 'widgets_init', 'takuchan__s_widgets_init' );
+add_action('widgets_init', 'takuchan__s_widgets_init');
 
 /**
  * Enqueue scripts and styles.
  */
-function takuchan__s_scripts() {
-	wp_enqueue_style( 'takuchan__s-style', get_stylesheet_uri(), array(), _S_VERSION );
-	wp_style_add_data( 'takuchan__s-style', 'rtl', 'replace' );
+function takuchan__s_scripts()
+{
+	wp_enqueue_style('takuchan__s-style', get_stylesheet_uri(), array(), _S_VERSION);
+	wp_style_add_data('takuchan__s-style', 'rtl', 'replace');
 
-	wp_enqueue_script( 'takuchan__s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
+	wp_enqueue_script('takuchan__s-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true);
 
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
+	if (is_singular() && comments_open() && get_option('thread_comments')) {
+		wp_enqueue_script('comment-reply');
 	}
 }
-add_action( 'wp_enqueue_scripts', 'takuchan__s_scripts' );
+add_action('wp_enqueue_scripts', 'takuchan__s_scripts');
 
 /**
  * Implement the Custom Header feature.
@@ -172,7 +196,6 @@ require get_template_directory() . '/inc/customizer.php';
 /**
  * Load Jetpack compatibility file.
  */
-if ( defined( 'JETPACK__VERSION' ) ) {
+if (defined('JETPACK__VERSION')) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
-
